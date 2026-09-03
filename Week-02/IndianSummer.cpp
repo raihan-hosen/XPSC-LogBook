738df2f89a779e20c7141103494d49cdecd8f10c
raihan-hosen <your_github_email@example.com>
2026-09-03 16:53:03 +0600

    Modify

--- Week-02/IndianSummer.cpp
+++ Week-02/IndianSummer.cpp
@@ -23,28 +23,3 @@ int main()
     solve();
     return 0;
 }
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-    int n;
-    cin >> n;
-    cin.ignore();
-
-    set<string> leaves;
-
-    for (int i = 0; i < n; i++)
-    {
-        string line;
-        getline(cin, line);
-        leaves.insert(line);
-    }
-    cout << leaves.size() << endl;
-}
-
-int main()
-{
-    solve();
-    return 0;
-}
