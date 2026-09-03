985b5b3d6e4b5291c1a8d27c361c7f2e3e06ff3a
raihan-hosen <your_github_email@example.com>
2026-09-03 16:39:53 +0600

    pull new files

---
+++ Week-02/IndianSummer.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    set<string> leaves;
+
+    for (int i = 0; i < n; i++)
+    {
+        string line;
+        getline(cin, line);
+        leaves.insert(line);
+    }
+    cout << leaves.size() << endl;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
